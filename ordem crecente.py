c4aca6955b88b01fa6127eb0fd4becff3c6d9cb2
Moisés <Pacocadxxie@gmail.com>
2025-12-16 18:59:32 -0300

    Add files via upload

---
+++ ordem crecente.py
@@ -0,0 +1,14 @@
+numero = list (range(10))
+
+for n in range (0,10):
+    numero[n] =int(input("digite um numero:"))
+
+for i in range(0,10):
+    for j in range(0,i):
+        if (numero [i] < numero[j]):
+            aux = numero [i]
+            numero [i] = numero [j]
+            numero[j] = aux
+
+for n in range(0,10): 
+    print(numero[n])            
