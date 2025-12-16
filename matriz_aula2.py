da52b24b57a85f26e7a41a576f0c7f81c4326018
Moisés <Pacocadxxie@gmail.com>
2025-12-16 20:20:32 -0300

    Add files via upload

---
+++ matriz_aula2.py
@@ -0,0 +1,9 @@
+numeros = [[0,0,0,0],
+           [0,0,0,0],
+           [0,0,0,0]]
+
+for i in range (0,3):
+    for j in range (0,4):
+        numeros [i] [j] = float(input('digite um numero:'))
+print(numeros[0][0])
+print(numeros[2][3])            
